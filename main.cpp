b96250302392ae1b4c47b39f51ea8b90233c5ecc
U-H470-0009\BTH <H470-0009+BTH@H470-0009.students.ad.bth.se>
2018-03-01 14:28:08 +0100

    Backup

--- main.cpp
+++ main.cpp
@@ -1,71 +1,155 @@
 #include <SFML/Graphics.hpp>
 #include <cmath>
 #include <iostream>
+#include <Windows.h>
 
 #include <glm.hpp>
-#define WIDTH 1280
-#define HEIGHT 720
+#define WIDTH 1280.0f
+#define HEIGHT 720.0f
+#define Radius 50
 
-void checkBounds(sf::CircleShape &shape, sf::Vector2f& pos, sf::Vector2f& direction)
+class Ball
 {
-	if ((pos.x + shape.getRadius()) > WIDTH || (pos.x - shape.getRadius()) < 0)
+private:
+	sf::CircleShape shape;
+	float mass;
+	glm::vec2 velocity;
+	glm::vec2 position;
+
+public:
+
+	Ball(glm::vec2 pos)
 	{
-		direction.x *= -1;
+		this->shape = sf::CircleShape(Radius);
+		shape.setOrigin(Radius, Radius);
+		shape.setPosition(sf::Vector2f(pos.x, pos.y));
+		
+		position = pos;
+		velocity = glm::vec2(0, 0);
+		mass = 50;
+	}
+
+	void setColor(sf::Color color)
+	{
+		shape.setFillColor(color);
+	}
+
+	void setPosition(glm::vec2 position)
+	{
+		this->position = position;
+		shape.setPosition(position.x, position.y);
+	}
+
+	glm::vec2 getVelocity()
+	{
+		return velocity;
+	}
+
+	void setVelocity(glm::vec2 velocity)
+	{
+		this->velocity = velocity;
+	}
+
+	glm::vec2 getPos()
+	{
+		return position;
+	}
+
+	void setMass(float mass)
+	{
+		this->mass = mass;
+	}
+	float getMass()
+	{
+		return mass;
+	}
+
+	sf::CircleShape getShape()
+	{
+		return shape;
+	}
+};
+
+
+void checkBounds(Ball& ball)
+{
+	if (GetAsyncKeyState(int('C')))
+		__debugbreak();
+	if ((ball.getPos().x + Radius) > WIDTH || (ball.getPos().x - Radius) < 0)
+	{
+		ball.setVelocity(glm::vec2(ball.getVelocity().x * -1, ball.getVelocity().y));
 	}
 
 
-	if ((pos.y + shape.getRadius()) > HEIGHT || (pos.y - shape.getRadius() < 0))
+	if ((ball.getPos().y + Radius) > HEIGHT || ((ball.getPos().y - Radius < 0)))
 	{
-		direction.y *= -1;
+		ball.setVelocity(glm::vec2(ball.getVelocity().x, ball.getVelocity().y * -1));
 	}
 }
 
-bool checkCollison(sf::CircleShape& shape1, sf::CircleShape& shape2)
+bool checkCollison(Ball ball1, Ball ball2)
 {
-	float s1X = shape1.getPosition().x;
-	float s1Y = shape1.getPosition().y;
 
-	float s2X = shape2.getPosition().x;
-	float s2Y = shape2.getPosition().y;
+	float distance = glm::length(ball1.getPos() - ball2.getPos());
 
-	float distanceBetween = sqrt(((s2X - s1X) * (s2X - s1X)) + ((s2Y - s1Y) * (s2Y - s1Y)));
-	float combinedRadius = shape1.getRadius() + shape2.getRadius();
-	
-	return distanceBetween <= combinedRadius;
+	return distance <= Radius*2;
 }
+
+bool move(Ball& ball, Ball& ball2)
+{
+	bool Collision = false;
+	glm::vec2 currentPos = ball.getPos();
+
+	glm::vec2 nextPos = ball.getPos() + ball.getVelocity();
+
+	float distance = glm::length(nextPos - currentPos);
+
+	float precision = 10.0f;
+
+	for (float i = distance / precision; i < distance && !Collision; i += distance / precision)
+	{
+		glm::vec2 pos = ball.getPos() + ((ball.getVelocity() / precision) * i);
+
+		float distanceBet = glm::length(ball2.getPos() - pos);
+		Collision = distanceBet < Radius * 2;
+		if (!Collision)
+		{
+			ball.setPosition(pos);
+		}
+
+	}
+
+	return Collision;
+}
+
+
+
 const float G = 0.00982f;
 int main()
 {
-	static float WhiteMass = 10;
-	static float YellowMass = 20;
+	Ball white(glm::vec2(WIDTH / 2.0f, HEIGHT / 2.0f));
+    white.setColor(sf::Color::White);
+	white.setVelocity(glm::vec2(1, 0));
+	white.setMass(25);
+	
+
+	Ball yellow(glm::vec2(80.0f, (HEIGHT / 2) + 50.0f));
+	yellow.setColor(sf::Color::Yellow);
+	yellow.setVelocity(glm::vec2(0,1));
+	yellow.setMass(120.0f);
+
 	bool Collision = false;
-	float my = -0.2f;
-	float WhiteAcceleration = (my * G);
-	float YellowAcceleration = (my * G);
+
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "SFML works!");
 
-    sf::CircleShape whiteBall(50.f);
-
-	whiteBall.setOrigin(sf::Vector2f(whiteBall.getRadius(), whiteBall.getRadius()));
-
-	sf::Vector2f whitePos(WIDTH/2,HEIGHT/2);
-	sf::Vector2f whiteDir(5.0f, 0.0f);
-
-	whiteBall.setPosition(whitePos);
-    whiteBall.setFillColor(sf::Color::White);
-
-	sf::CircleShape yellowBall(50.0f);
-
-	yellowBall.setOrigin(sf::Vector2f(yellowBall.getRadius(), yellowBall.getRadius()));
-
-	sf::Vector2f yellowPos(80.0f,( HEIGHT/2) + 50.0f);
-	sf::Vector2f yellowDir(0.0f, 0.0f);
-
-	yellowBall.setPosition(yellowPos);
-	yellowBall.setFillColor(sf::Color::Yellow);
 
     while (window.isOpen())
     {
+		if (GetAsyncKeyState(VK_SPACE))
+		{
+			white.setVelocity(glm::vec2(1, 0));
+			yellow.setVelocity(glm::vec2(0, 1));
+		}
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -73,122 +157,118 @@ int main()
                 window.close();
         }
 		
-		checkBounds(whiteBall, whitePos, whiteDir);
-		checkBounds(yellowBall, yellowPos, yellowDir);
-		/*if (whiteDir.x > -WhiteAcceleration)
-		{
-			whiteDir.x += WhiteAcceleration;
-		}
-		else if(whiteDir.x < WhiteAcceleration)
-		{
-			whiteDir.x -= WhiteAcceleration;
-		}
-		else
-		{
-			whiteDir.x = 0;
-		}
-		if (whiteDir.y > -WhiteAcceleration)
-		{
-			whiteDir.y += WhiteAcceleration;
-		}
-		else if (whiteDir.y < WhiteAcceleration)
-		{
-			whiteDir.y -= WhiteAcceleration;
-		}
-		else
-		{
-			whiteDir.y = 0;
-		}*/
-		//yellowDir.x += YellowAcceleration;
-		//yellowDir.y += YellowAcceleration;
+		checkBounds(white);
+		checkBounds(yellow);
 
 		Collision = false;
 		
-		for(int i = 0; i < whiteDir.x && Collision == false || i < whiteDir.y && Collision == false;i++)
-		{
-			if (i < whiteDir.x)
-			{
-				whitePos.x += 1;
-			}
-			if (i < whiteDir.y)
-			{
-				whitePos.y += 1;
-			}
-			whiteBall.setPosition(whitePos);
-			Collision = checkCollison(whiteBall, yellowBall);
-		}
-		for (int i = 0; i < yellowDir.x && Collision == false || i < yellowDir.y && Collision == false; i++)
-		{
-			if (i < yellowDir.x)
-			{
-				yellowPos.x += 1;
-			}
-			if (i < yellowDir.y)
-			{
-				yellowPos.y += 1;
-			}
-			yellowBall.setPosition(yellowPos);
-			Collision = checkCollison(whiteBall, yellowBall);
-		}
-		for (int i = 0; i > whiteDir.x && Collision == false || i > whiteDir.y && Collision == false; i--)
-		{
-			if (i > whiteDir.x)
-			{
-				whitePos.x -= 1;
-			}
-			if (i > whiteDir.y)
-			{
-				whitePos.y -= 1;
-			}
-			whiteBall.setPosition(whitePos);
-			Collision = checkCollison(whiteBall, yellowBall);
-		}
-		for (int i = 0; i > yellowDir.x && Collision == false || i > yellowDir.y && Collision == false; i--)
-		{
-			if (i > yellowDir.x)
-			{
-				yellowPos.x -= 1;
-			}
-			if (i > yellowDir.y)
-			{
-				yellowPos.y -= 1;
-			}
-			Collision = checkCollison(whiteBall, yellowBall);
-			yellowBall.setPosition(yellowPos);
-		}
+		Collision = move(white, yellow);
+		Collision = move(yellow, white);
+
+
+		//for(int i = 0; i < white.getVelocity().x && Collision == false || i < white.getVelocity().y && Collision == false;i++)
+		//{
+		//	if (i < white.getVelocity().x)
+		//	{
+		//		
+		//		whitePos.x += 1;
+		//	}
+		//	if (i < whiteDir.y)
+		//	{
+		//		whitePos.y += 1;
+		//	}
+		//	sf::Vector2f oldPos = whiteBall.getPosition();
+		//	
+		//	Collision = checkCollison(whiteBall, yellowBall);
+		//	if(Collision)
+		//		whiteBall.setPosition(oldPos);
+		//}
+		//for (int i = 0; i < yellowDir.x && Collision == false || i < yellowDir.y && Collision == false; i++)
+		//{
+		//	if (i < yellowDir.x)
+		//	{
+		//		yellowPos.x += 1;
+		//	}
+		//	if (i < yellowDir.y)
+		//	{
+		//		yellowPos.y += 1;
+		//	}
+		//	sf::Vector2f oldPos = yellowBall.getPosition();
+		//	yellowBall.setPosition(yellowPos);
+		//	Collision = checkCollison(whiteBall, yellowBall);
+		//	if (Collision)
+		//		yellowBall.setPosition(oldPos);
+		//}
+		//for (int i = 0; i > whiteDir.x && Collision == false || i > whiteDir.y && Collision == false; i--)
+		//{
+		//	if (i > whiteDir.x)
+		//	{
+		//		whitePos.x -= 1;
+		//	}
+		//	if (i > whiteDir.y)
+		//	{
+		//		whitePos.y -= 1;
+		//	}
+		//	sf::Vector2f oldPos = whiteBall.getPosition();
+		//	whiteBall.setPosition(whitePos);
+		//	Collision = checkCollison(whiteBall, yellowBall);
+		//	if (Collision)
+		//		whiteBall.setPosition(oldPos);
+		//}
+		//for (int i = 0; i > yellowDir.x && Collision == false || i > yellowDir.y && Collision == false; i--)
+		//{
+		//	if (i > yellowDir.x)
+		//	{
+		//		yellowPos.x -= 1;
+		//	}
+		//	if (i > yellowDir.y)
+		//	{
+		//		yellowPos.y -= 1;
+		//	}
+		//	sf::Vector2f oldPos = whiteBall.getPosition();
+		//	yellowBall.setPosition(yellowPos);
+		//	Collision = checkCollison(whiteBall, yellowBall);
+		//	if (Collision)
+		//		yellowBall.setPosition(oldPos);
+		//}
 
 		if (Collision)
 		{
-			static float e = 0.2;
+			static float e = 0.5;
 
 
-			glm::vec2 gWhiteDir = (glm::vec2(whiteDir.x, whiteDir.y));
-			glm::vec2 gYellowDir = (glm::vec2(yellowDir.x, yellowDir.y));
+			glm::vec2 gWhiteDir = (glm::vec2(white.getVelocity().x, white.getVelocity().y));
+			glm::vec2 gYellowDir = (glm::vec2(yellow.getVelocity().x, yellow.getVelocity().y));
 
-			glm::vec2 gWhitePos = glm::vec2(whitePos.x, whitePos.y);
-			glm::vec2 gYellowPos = glm::vec2(yellowPos.x, yellowPos.y);
+			glm::vec2 gWhitePos = glm::vec2(white.getPos().x, white.getPos().y);
+			glm::vec2 gYellowPos = glm::vec2(yellow.getPos().x, yellow.getPos().y);
 
-			glm::vec2 d = gWhitePos - gYellowPos;
+			glm::vec2 d = glm::normalize(gWhitePos - gYellowPos);
 
-			glm::vec2 WhiteV0 = glm::dot(gWhiteDir, d) * glm::normalize(d);
-			glm::vec2 YellowV0 = glm::dot(gYellowDir, d) * glm::normalize(d);
+			glm::vec2 WhiteV0 = glm::dot(gWhiteDir, d) * d;
+			glm::vec2 YellowV0 = glm::dot(gYellowDir, d) * d;
+
+			float WhiteMass = white.getMass();
+			float YellowMass = yellow.getMass();
 
 			glm::vec2 WhiteV1 = (WhiteV0*(WhiteMass - (YellowMass*e)) + (YellowMass * YellowV0 *(1 + e))) / (WhiteMass + YellowMass);
 			glm::vec2 YellowV1 = (YellowV0*(YellowMass - (WhiteMass*e)) + (WhiteMass*WhiteV0*(1 + e))) / (WhiteMass + YellowMass);
 
 			glm::vec2 WhiteFinal = gWhiteDir - WhiteV0 + WhiteV1;
 			glm::vec2 YellowFinal = gYellowDir - YellowV0 + YellowV1;
-			WhiteFinal = glm::normalize(WhiteFinal);
-			YellowFinal = glm::normalize(YellowFinal);
+			WhiteFinal = (WhiteFinal);
+			YellowFinal = (YellowFinal);
 
-			std::cout << "WhiteBall Direction: before: (" << gWhiteDir.x << "," << gWhiteDir.y << ")" << " After: (" << WhiteFinal.x << "," << WhiteFinal.y << ")\n";
+			/*std::cout << "WhiteBall Direction: before: (" << gWhiteDir.x << "," << gWhiteDir.y << ")" << " After: (" << WhiteFinal.x << "," << WhiteFinal.y << ")\n";
 			std::cout << "WhiteBall Position: (" << whitePos.x << "," << whitePos.y << ")" << std::endl;
 
 			std::cout << "\nYellowBall Direction: before: (" << gYellowDir.x << "," << gYellowDir.y << ")" << " After: (" << YellowFinal.x << "," << YellowFinal.y << ")\n";
-			std::cout << "YellowBall Position: (" << yellowPos.x << "," << yellowPos.y << ")" << std::endl;
+			std::cout << "YellowBall Position: (" << yellowPos.x << "," << yellowPos.y << ")" << std::endl;*/
 
-			whiteDir = sf::Vector2f(WhiteFinal.x, WhiteFinal.y);
-			yellowDir = sf::Vector2f(YellowFinal.x, YellowFinal.y);
+			white.setVelocity(WhiteFinal);
+			yellow.setVelocity(YellowFinal);
+			
+
 
 
 
@@ -199,8 +279,8 @@ int main()
 
 
         window.clear();
-		window.draw(yellowBall);
-        window.draw(whiteBall);
+		window.draw(white.getShape());
+        window.draw(yellow.getShape());
         window.display();
     }
 
